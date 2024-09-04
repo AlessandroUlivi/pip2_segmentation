07f5a2a6444d440d3ae67814feaeb56c473948f6
Alessandro Ulivi <ale.ulivi@gmail.com>
2024-09-04 16:08:55 +0200

    add check_files_else_make_folders functions to utils.py

--- utils.py
+++ utils.py
@@ -9,3 +9,32 @@ def listdirNHF(input_directory):
     output: list of elements in the input-folder, except hidden files. 
     """
     return [f for f in os.listdir(input_directory) if not f.startswith(".")]
+
+
+def check_files_else_make_folders(folder_dir_to_create):
+    """
+    Check if a folder is present and if it contains any object. If the folder is not present, it creates it.
+    inputs:
+    - the directory of a folder to check
+    outputs:
+    - False if the folder is not present.
+    - False if the folder is present and it is empty
+    - True is the folder is present and it is not empty
+    """
+    #if the folder does not exist
+    if not os.path.exists(folder_dir_to_create):
+        os.makedirs(folder_dir_to_create)
+        #return False as no file is present in the folder
+        return False
+    #if the folder exists
+    else:
+        #get a list of files in the folder
+        folders_files_list = listdirNHF(folder_dir_to_create)
+        #if files are present in the folder
+        if len(folders_files_list)>0:
+            #return True as files are present in the folder
+            return True
+        #if no files are present in the folder
+        else:
+            #return False as no file is present in the folder
+            return False
