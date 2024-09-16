593604373d9822884f0c3c3416e467b957266b62
Alessandro Ulivi <ale.ulivi@gmail.com>
2024-09-16 23:52:53 +0200

    fix crop_spatial_dimensions in utils.py

--- utils.py
+++ utils.py
@@ -66,14 +66,19 @@ def crop_spatial_dimensions(x, y, x_dim=[-2,-1], y_dim=[-2,-1]):
         It is assumed that no dimension of y has size > than the corresponding dimension in x.
         """
 
+        #get the desired output size by joining all the dimensions of x (input) before the last 2 (before YX)
+        #with the YX dimension (the last 2 dimensions of the size) of x (input)
+        x_target_size = x.size()[:-2] + y.size()[-2:]
+
         #per each dimension, get the how many pixels should be added/removed (offset) from x in order to match y.
         #Note: the offset is divided by 2 because it will be added/removed from x to both sides of each dimension.
-        offset = tuple((a - b) // 2 for a, b in zip((x.shape[x_dim[0]],x.shape[x_dim[1]]), (y.shape[y_dim[0]],y.shape[y_dim[1]])))
+        offset = tuple((a - b) // 2 for a, b in zip(x.size(), x_target_size))
 
         #create a tuple of "slice" objects, with one object per dimension of the inputs.
         #each slice indicates the beginning and end (initial and final indeces) of the part of x which should be kept.
         #NOTE Because each slice starts at the index 'offset' and ends at the index 'offset + size of y' the process effectively guarantees
         #that the central part of x is maintened and matches y.
-        slices = tuple(slice(o, o + s) for o, s in zip(offset, (y.shape[y_dim[0]],y.shape[y_dim[1]])))
-        
+        slices = tuple(slice(o, o + s) for o, s in zip(offset, x_target_size))
+
         return x[slices]
+
