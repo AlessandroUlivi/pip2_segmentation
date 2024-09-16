18dab52bdceca5b164719166f764872e38a044d1
Alessandro Ulivi <ale.ulivi@gmail.com>
2024-09-16 23:34:05 +0200

    allow definition of starting index in crop_spatial_dimensions within utils.py

--- utils.py
+++ utils.py
@@ -59,20 +59,21 @@ def get_random_image_label_pair(images, labels):
 
     return images[random_indx, ...], labels[random_indx, ...]
 
-def crop_spatial_dimensions(x, y):
+def crop_spatial_dimensions(x, y, x_dim=[-2,-1], y_dim=[-2,-1]):
         """
-        Center-crop x to match spatial dimensions given by y. Note: for spatial dimensions are meant the xy image dimension, which expected in positions
-        -1 and -2. It is assumed that no dimension of y has size > than the corresponding dimension in x.
+        Center-crop x to match spatial dimensions given by y. Note: spatial dimensions the xy image dimension, their positions within x and y shapes can be
+        expressed using x_dim and y_dim. By default they are expected in positions -1 and -2.
+        It is assumed that no dimension of y has size > than the corresponding dimension in x.
         """
 
         #per each dimension, get the how many pixels should be added/removed (offset) from x in order to match y.
         #Note: the offset is divided by 2 because it will be added/removed from x to both sides of each dimension.
-        offset = tuple((a - b) // 2 for a, b in zip(x.shape[-2:], y.shape[-2:]))
-
+        offset = tuple((a - b) // 2 for a, b in zip(x.shape[x_dim[0]:x_dim[1]], y.shape[y_dim[0],y_dim[1]]))
+        print(offset)
         #create a tuple of "slice" objects, with one object per dimension of the inputs.
         #each slice indicates the beginning and end (initial and final indeces) of the part of x which should be kept.
         #NOTE Because each slice starts at the index 'offset' and ends at the index 'offset + size of y' the process effectively guarantees
         #that the central part of x is maintened and matches y.
-        slices = tuple(slice(o, o + s) for o, s in zip(offset, y.shape[-2:]))
+        slices = tuple(slice(o, o + s) for o, s in zip(offset, y.shape[y_dim[0],y_dim[1]]))
 
         return x[slices]
