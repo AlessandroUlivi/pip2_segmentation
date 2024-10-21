cea888760fe0bd031624700a50762021a90c79f2
Alessandro Ulivi <ale.ulivi@gmail.com>
2024-10-21 14:57:57 +0200

    start creating run_training_no_val in train_model.py

--- train_model.py
+++ train_model.py
@@ -10,21 +10,20 @@ from test_model import test_model
 import torch.optim as optim
 
 
-def train(
-    model,
-    loader,
-    optimizer,
-    loss_function,
-    epoch,
-    log_interval=100,
-    log_image_interval=20,
-    tb_logger=None,
-    device=None,
-    x_dim=[-2,-1],
-    y_dim=[-2,-1],
-    return_loss_metric=False,
-    metric=None,
-    bin_threshold=None):
+def train(model,
+          loader,
+          optimizer,
+          loss_function,
+          epoch,
+          log_interval=100,
+          log_image_interval=20,
+          tb_logger=None,
+          device=None,
+          x_dim=[-2,-1],
+          y_dim=[-2,-1],
+          return_loss_metric=False,
+          metric=None,
+          bin_threshold=None):
     """
     train the model for 1 epoch. Exploits TensorBoard to keep track of the training progress (variation of the loss function).
 
@@ -210,7 +209,7 @@ def run_training(model,
     - logger. TensorBoard logger. To keep track of progress. Refer to https://www.tensorflow.org/tensorboard?hl=it
     - log_interval. Int. Optional. Default 100. After how many batches the TensorBoard console is updated by saving the loss function,
     in order to track the progression of the training process.
-     - log_image_interval. Int. Optional. Default 20. After how many batches the TensorBoard console is updated by saving the prediction results,
+    - log_image_interval. Int. Optional. Default 20. After how many batches the TensorBoard console is updated by saving the prediction results,
     in order to track the progression of the training process.
     - device. Optional. None or device. Default None. The device to use for the training. If None, it will automatically checked if a cuda gpu is available.
     If available, it will be used. If not available, the cpu will be used.
@@ -246,21 +245,20 @@ def run_training(model,
     # train for n_epochs. During the training inspect the predictions
     for epoch in range(n_epochs):
         # train the model
-        train(
-            model=model,
-            loader=train_loader,
-            optimizer=optimizer,
-            loss_function=loss_function,
-            epoch=epoch,
-            log_interval=log_interval,
-            log_image_interval=log_image_interval,
-            tb_logger=logger,
-            device=device,
-            x_dim=x_dim,
-            y_dim=y_dim,
-            return_loss_metric=False,
-            metric=None,
-            bin_threshold=None)
+        train(model=model,
+              loader=train_loader,
+              optimizer=optimizer,
+              loss_function=loss_function,
+              epoch=epoch,
+              log_interval=log_interval,
+              log_image_interval=log_image_interval,
+              tb_logger=logger,
+              device=device,
+              x_dim=x_dim,
+              y_dim=y_dim,
+              return_loss_metric=False,
+              metric=None,
+              bin_threshold=None)
 
         #calculate the training step
         step = epoch * len(train_loader)
@@ -287,102 +285,115 @@ def run_training(model,
         if len(path)>0 and current_metric>best_metric:
             save_checkpoint(model, optimizer, epoch, path, key)
 
-# def run_training_no_val(model,
-#                         optimizer,
-#                         metric,
-#                         n_epochs,
-#                         train_loader,
-#                         loss_function,
-#                         bin_threshold=0.5,
-#                         logger=None,
-#                         log_interval=100,
-#                         device=None,
-#                         key="checkpoint",
-#                         path="",
-#                         lr_scheduler_flag = False,
-#                         lr_kwargs={"mode":"min", "factor": 0.1, "patience":2},
-#                         x_dim=[-2,-1],
-#                         y_dim=[-2,-1]):
+
+def run_training_no_val(model,
+                        optimizer,
+                        metric,
+                        n_epochs,
+                        train_loader,
+                        loss_function,
+                        bin_threshold=0.5,
+                        logger=None,
+                        log_interval=100,
+                        log_image_interval=20,
+                        device=None,
+                        key="checkpoint",
+                        path="",
+                        lr_scheduler_flag = False,
+                        lr_kwargs={"mode":"min", "factor": 0.1, "patience":2},
+                        x_dim=[-2,-1],
+                        y_dim=[-2,-1],
+                        best_metric_init = 0):
     
-#     # log_image_interval=20,
-#     # tb_logger=None,
-#     # device=None,
-#     # x_dim=[-2,-1],
-#     # y_dim=[-2,-1],
-#     # return_loss_metric=False,
-#     # metric=None,
-#     # bin_threshold=None
+    """
+    trains a model over multiple epochs without validation. This function can be used to train a final model using the full data (train data + val data
+    + test data).
+    Exploits TensorBoard to keep track of the training progress (variation of the loss function).
+
+    NOTE: checkpoints are saved based on the the average matric obtained at each training epoch: if the metric improves a checkpoint is saved. As the
+    metric is calculated on the training data, this procedure is at risk of saving a models overfitting the data. Thus this functions must be run on an
+    adequate number of epochs, established not to overfit.
+
+    Inputs:
+    - model. The model to train. Must be derived from torch.nn.Module.
+    - optimizer. The optimizer of the training process. An object from PyTorch is expected. Refer to https://pytorch.org/docs/stable/optim.html
+    - metric. The metric to use to evaluate the training process.
+    - n_epochs. Int. The number of epochs to use for the training process.
+    - train_loader. Train data organized in minibatches for the epoch (with inputs and labels). A DataLoader object form torch.utils.data is expected. Refer to https://pytorch.org/tutorials/beginner/basics/data_tutorial.html
+    - loss_function. The loss_function of the training processs. An object from PyTorch is expected. Refer to https://pytorch.org/docs/stable/nn.html#loss-functions
+    - bin_threshold. Float. Optional. Default 0.5. The value to use as highpass threshold for binarizing the predicted image before calculating the metric.
+    - logger. TensorBoard logger. To keep track of progress. Refer to https://www.tensorflow.org/tensorboard?hl=it
+    - log_interval. Int. Optional. Default 100. After how many batches the TensorBoard console is updated by saving the loss function,
+    in order to track the progression of the training process.
+    - log_image_interval. Int. Optional. Default 20. After how many batches the TensorBoard console is updated by saving the prediction results,
+    in order to track the progression of the training process.
+    - device. Optional. None or device. Default None. The device to use for the training. If None, it will automatically checked if a cuda gpu is available.
+    If available, it will be used. If not available, the cpu will be used.
+    - key. String. Optional. Default 'checkpoint'. The key to use for saving the checkpoint.
+    - path. String. Optional. Default "" (empty string). If empty string, no checkpoints are saved during the training process. If different than an empty string,
+    the provided string will be used as directory to save checkpoints. An error is returned if the directory doesn't exist.
+    - lr_scheduler_flag. Bool. Optional. Default False. If True, torch.optim.lr_scheduler.ReduceLROnPlateau will be used as learning rate scheduler. No learning scheduler will be used otherwise.
+    - lr_kwargs. Dictionary. Optional. Default {"mode":"min", "factor": 0.1, "patience":2}. The kwargs parameters to be passed to torch.optim.lr_scheduler.ReduceLROnPlateau if lr_scheduler_flag==True.
+    - x_dim. List of int. Optional. Default [-2, -1]. The position of Y and X axes in x input image. The parameter is passed to the x_dim input in crop_spatial_dimensions.
+    - y_dim. List of int. Optional. Default [-2, -1]. The position of Y and X axes in y input image. The parameter is passed to the y_dim input in crop_spatial_dimensions.
+    - best_metric_init. Int or float. Optional. Default 0. The value to use for initializing the best_metric parameter.
+
+    The function has no output.
+    """
     
-#     """
-#     trains a model over multiple epochs without validation. This function can be used to train a final model using the full data (train data + val data
-#     + test data).
-#     Exploits TensorBoard to keep track of the training progress (variation of the loss function).
+    #if no device is passed, check if the gpu is available, else use the cpu
+    if device is None:
+        if torch.cuda.is_available():
+            device = torch.device("cuda")
+        else:
+            device = torch.device("cpu")
 
-#     NOTE: checkpoints are saved based on the the average matric obtained at each training epoch: if the metric improves a checkpoint is saved. As the
-#     metric is calculated on the training data, this procedure is at risk of saving a models overfitting the data. Thus this functions must be run on an
-#     adequate number of epochs, established not to overfit.
+    # # send model to device
+    # model = model.to(device)
 
-#     Inputs:
-#     - model. The model to train. Must be derived from torch.nn.Module.
-#     - optimizer. The optimizer of the training process. An object from PyTorch is expected. Refer to https://pytorch.org/docs/stable/optim.html
-#     - metric. The metric to use to evaluate the training process.
-#     - n_epochs. Int. The number of epochs to use for the training process.
-#     - train_loader. Train data organized in minibatches for the epoch (with inputs and labels). A DataLoader object form torch.utils.data is expected. Refer to https://pytorch.org/tutorials/beginner/basics/data_tutorial.html
-#     - loss_function. The loss_function of the training processs. An object from PyTorch is expected. Refer to https://pytorch.org/docs/stable/nn.html#loss-functions
-#     - bin_threshold. Float. Optional. Default 0.5. The value to use as highpass threshold for binarizing the predicted image before calculating the metric.
-#     - logger. TensorBoard logger. To keep track of progress. Refer to https://www.tensorflow.org/tensorboard?hl=it
-#     - log_interval. Int. Optional. Default 100. After how many batches the TensorBoard console is updated by saving the loss function,
-#     in order to track the progression of the training process.
-#     - device. Optional. None or device. Default None. The device to use for the training. If None, it will automatically checked if a cuda gpu is available.
-#     If available, it will be used. If not available, the cpu will be used.
-#     - key. String. Optional. Default 'checkpoint'. The key to use for saving the checkpoint.
-#     - path. String. Optional. Default "" (empty string). If empty string, no checkpoints are saved during the training process. If different than an empty string,
-#     the provided string will be used as directory to save checkpoints. An error is returned if the directory doesn't exist.
-#     - lr_scheduler_flag. Bool. Optional. Default False. If True, torch.optim.lr_scheduler.ReduceLROnPlateau will be used as learning rate scheduler. No learning scheduler will be used otherwise.
-#     - lr_kwargs. Dictionary. Optional. Default {"mode":"min", "factor": 0.1, "patience":2}. The kwargs parameters to be passed to torch.optim.lr_scheduler.ReduceLROnPlateau if lr_scheduler_flag==True.
-#     - x_dim. List of int. Optional. Default [-2, -1]. The position of Y and X axes in x input image. The parameter is passed to the x_dim input in crop_spatial_dimensions.
-#     - y_dim. List of int. Optional. Default [-2, -1]. The position of Y and X axes in y input image. The parameter is passed to the y_dim input in crop_spatial_dimensions.
+    #initialize the learning scheduler, if specified
+    if lr_scheduler_flag:
+        lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, **lr_kwargs)
 
-#     The function has no output.
-#     """
-    
-#     #if no device is passed, check if the gpu is available, else use the cpu
-#     if device is None:
-#         if torch.cuda.is_available():
-#             device = torch.device("cuda")
-#         else:
-#             device = torch.device("cpu")
+    #initialize the best_metric parameter - it will be used for saving checkpoints
+    best_metric = best_metric_init
 
-#     # # send model to device
-#     # model = model.to(device)
+    # train for n_epochs. During the training inspect the predictions
+    for epoch in range(n_epochs):
+        # train the model
+        current_loss, current_metric = train(model=model,
+                                             loader=train_loader,
+                                             optimizer=optimizer,
+                                             loss_function=loss_function,
+                                             epoch=epoch,
+                                             log_interval=log_interval,
+                                             log_image_interval=log_image_interval,
+                                             tb_logger=logger,
+                                             device=device,
+                                             x_dim=x_dim,
+                                             y_dim=y_dim,
+                                             return_loss_metric=True,
+                                             metric=metric,
+                                             bin_threshold=bin_threshold)
 
-#     #initialize the learning scheduler, if specified
-#     if lr_scheduler_flag:
-#         lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, **lr_kwargs)
+        #calculate the training step
+        step = epoch * len(train_loader)
 
-#     # train for n_epochs. During the training inspect the predictions
-#     for epoch in range(n_epochs):
-#         # train the model
-#         current_loss, current_metric = train(model=model,
-#                                              loader=train_loader,
-#                                              optimizer=optimizer,
-#                                              loss_function=loss_function,
-#                                              epoch=epoch,
-#                                              log_interval=log_interval,
-#                                              tb_logger=logger,
-#                                              device=device,
-#                                              x_dim=x_dim,
-#                                              y_dim=y_dim)
-
-#         #calculate the training step
-#         step = epoch * len(train_loader)
-
-#         # update the learning scheduler if it is provided
-#         if lr_scheduler_flag:
-#             lr_scheduler.step(current_loss)
-#             logger.add_scalar(tag="lr", scalar_value=lr_scheduler.get_last_lr()[0], global_step=step
-#             )
+        # update the learning scheduler if it is provided
+        if lr_scheduler_flag:
+            lr_scheduler.step(current_loss)
+            logger.add_scalar(tag="lr", scalar_value=lr_scheduler.get_last_lr()[0], global_step=step
+            )
         
-#         #save checkpoint if a path is specified and the metric is the best
-#         if len(path)>0 and current_metric>best_metric:
-#             save_checkpoint(model, optimizer, epoch, path, key)
+        # log to tensorboard if it is provided
+        if tb_logger is not None:
+            step = epoch * len(loader)
+            tb_logger.add_scalar(
+                tag="train_avg_metric", scalar_value=current_metric, global_step=step+len(train_loader)
+            )
+
+        #save checkpoint if a path is specified and the metric is the best
+        if len(path)>0 and current_metric>best_metric:
+            save_checkpoint(model, optimizer, epoch, path, key)
+
+
